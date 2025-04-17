8548e83adba833f1bb19a12ecb04d45771c8b898
Nikita <98713485+n1kitka@users.noreply.github.com>
2025-04-17 13:03:54 +0300

    Initial SwiftUI project setup

---
+++ ScrapBays/ScrapBays/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScrapBays
+//
+//  Created by Nikita on 17.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScrapBays/ScrapBays/ScrapBaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrapBaysApp.swift
+//  ScrapBays
+//
+//  Created by Nikita on 17.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrapBaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
